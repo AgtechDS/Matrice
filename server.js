a2eacbbd2f608f99ff65e416cd75cdfb8e6b99be
AgtechDS <agtechdesigne@gmail.com>
2026-08-21 17:31:20 +0200

    feat(seo): add robots.txt and sitemap.xml to streamline Google AdSense and Googlebot indexing

--- server.js
+++ server.js
@@ -636,6 +636,7 @@ const MIME_TYPES = {
     '.webp': 'image/webp',
     '.ico': 'image/x-icon',
     '.txt': 'text/plain; charset=utf-8',
+    '.xml': 'application/xml; charset=utf-8',
     '.webmanifest': 'application/manifest+json; charset=utf-8'
 };
 
