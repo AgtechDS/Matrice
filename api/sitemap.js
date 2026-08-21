1c083cc3aa222315fa618fc2a68af3c49764d13b
AgtechDS <agtechdesigne@gmail.com>
2026-08-21 17:32:09 +0200

    feat(seo): add edge endpoints and rewrites for robots.txt and sitemap.xml

---
+++ api/sitemap.js
@@ -0,0 +1,25 @@
+export default function handler(req, res) {
+    const content = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://matrice-jade.vercel.app/</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+    if (res && typeof res.setHeader === 'function') {
+        res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+        res.setHeader('Cache-Control', 'public, max-age=86400');
+        res.status(200).send(content);
+    } else {
+        return new Response(content, {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/xml; charset=utf-8',
+                'Cache-Control': 'public, max-age=86400'
+            }
+        });
+    }
+}
